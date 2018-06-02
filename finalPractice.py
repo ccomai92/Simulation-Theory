c1f2117da81db9ff5afcd110daca82de18e41673
Gihwan Kwon <gihwankwon@D-10-19-60-153.dhcp4.washington.edu>
2018-06-02 15:54:59 -0700

    from mac

---
+++ finalPractice.py
@@ -0,0 +1,52 @@
+# 1.
+# In a NumPy masked array, if an element of the mask is "True", 
+
+# answer: 
+# the corresponding element in the data is BAD or Missing data. 
+
+# 2. 
+# What is returned from the following code? 
+import numpy as np 
+print("Problem 2. ")
+a = np.array([3, 56, 1, 354, -2, 8, 10])
+print(a[3:])
+
+# answer: [354, -2, 8, 10]
+
+# 3. 
+# The von Neumann neighborhood to a cell includes 
+#
+# answer: 
+# all adjoining cells that share a vertex with the 
+# cell in question + which is 4 ways 
+# On the other had Moore neighborhood is 8 ways
+
+# 4.
+# Which of the following functions is used in matplotlib's 
+# pyplot module to create an x-y line plot? 
+#
+# answer: 
+# plot 
+
+# 5. 
+# A NumPy array differs from a Python list in that 
+#
+# answer: 
+# a NumPy array is fixed size while a Python list is not 
+# fixed in length 
+
+# 6. 
+# Consider following code  
+print("Problem 6.")
+a = np.zeros((4, 3))
+print(a)
+b = 5
+a = a + b 
+print(a)
+
+# ansnwer: 
+# [[5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]]
+
